a68eaa13bae1d078fe9942cba7198b43d59491bb
Johnmclf <johnmclf@gmail.com>
2025-06-18 11:03:27 +0200

    fin tp7

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -1,6 +1,71 @@
-﻿namespace BlazingPizza.Data
+﻿namespace BlazingPizza.Data;
+public static class SeedData
 {
-    public class SeedData
+ public static void Initialize(PizzaStoreContext db)
+{
+    var specials = new PizzaSpecial[]
     {
-    }
+ new PizzaSpecial()
+ {
+ Name= "BasicCheese Pizza",
+ Description =
+ "It'scheesy anddelicious. Whywouldn'tyou wantone?",
+ BasePrice =9.99m,
+ ImageUrl ="img/pizzas/cheese.jpg",
+ },
+ new PizzaSpecial()
+ {
+ Id= 2,
+ Name= "TheBaconatorizor",
+ Description ="IthasEVERYkind ofbacon",
+ BasePrice =11.99m,
+ ImageUrl = "img/pizzas/bacon.jpg",
+ },
+ new PizzaSpecial()
+ {
+ Id = 3,
+ Name = "Classic pepperoni",
+ Description =
+ "It's the pizza you grew up with, but Blazing hot!",
+ BasePrice = 10.50m,
+ ImageUrl = "img/pizzas/pepperoni.jpg",
+ },
+ new PizzaSpecial()
+ {
+ Id = 4,
+ Name = "Buffalo chicken",
+ Description = "Spicy chicken, hot sauce and bleu cheese, guaranteed to warm you up",
+ BasePrice = 12.75m,
+ ImageUrl = "img/pizzas/meaty.jpg",
+ },
+ new PizzaSpecial()
+ {
+ Id = 5,
+ Name = "Mushroom Lovers",
+ Description =
+ "It has mushrooms. Isn't that obvious?",
+ BasePrice = 11.00m,
+ ImageUrl = "img/pizzas/mushroom.jpg",
+ },
+ new PizzaSpecial()
+ {
+ Id = 7,
+ Name = "Veggie Delight",
+ Description = "It's like salad, but on a pizza",
+ BasePrice = 11.50m,
+ ImageUrl = "img/pizzas/salad.jpg",
+ },
+ new PizzaSpecial()
+ {
+ Id = 8,
+ Name = "Margherita",
+ Description =
+ "Traditional Italian pizza with tomatoes and basil",
+ BasePrice = 9.99m,
+ ImageUrl = "img/pizzas/margherita.jpg",
+ },
+ };
+    db.Specials.AddRange(specials);
+    db.SaveChanges();
 }
+ }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using BlazingPizza.Data;
+using BlazingPizza.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+builder.Services.AddScoped<OrderState>();
 
 var app = builder.Build();
 
@@ -16,5 +22,16 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// Initialize the database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
